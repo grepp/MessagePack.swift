652a7d974077d4b5ca49acecd0b39677fe014909
Hwakyoung Lee <hwakyoung.lee@gmail.com>
2019-06-19 11:01:47 +0900

    add products to package file

--- Package.swift
+++ Package.swift
@@ -3,6 +3,9 @@ import PackageDescription
 
 let package = Package(
     name: "MessagePack",
+    products: [
+        .library(name: "MessagePack", targets: ["MessagePack"]),
+    ],
     targets: [
         .target(name: "MessagePack", path: "./Sources"),
         .testTarget(name: "MessagePackTests", path: "./Tests")
